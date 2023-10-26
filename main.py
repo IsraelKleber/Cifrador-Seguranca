4f384e8a3f40307fedd32971132b1536477cf805
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2023-10-26 18:07:00 -0300

    wip: validação de senha no cifrador (#1)

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 app = FastAPI()
 
 class CifraRequest(BaseModel):
     texto: str
-    senha: str
+    senha: int
+
+    @validator('senha')
+    def validate_senha(senha):
+        senha_str = str(senha)
+        if not senha_str.isdigit():
+            raise ValueError("A senha deve conter apenas dígitos.")
+        return senha
+
 
 def cifra_texto(texto, senha):
+    senha_str = str(senha)
     resultado = []
-    senha_len = len(senha)
+    senha_len = len(senha_str)
     
     # Defina a ordem de cifragem desejada
     ordem_cifra = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ,.0123456789-"
     
     for i, char in enumerate(texto):
-        senha_char = senha[i % senha_len]
+        senha_char = senha_str[i % senha_len]
         offset = ordem_cifra.index(char)  # Obtem o índice na ordem_cifra
         cifra_char = offset + int(senha_char)
         x = ordem_cifra[cifra_char % len(ordem_cifra)]
@@ -24,14 +33,15 @@ def cifra_texto(texto, senha):
 
 
 def decifra_texto(texto, senha):
+    senha_str = str(senha)
     resultado = []
-    senha_len = len(senha)
+    senha_len = len(senha_str)
     
     # Defina a ordem de cifragem desejada
     ordem_cifra = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ,.0123456789-"
     
     for i, char in enumerate(texto):
-        senha_char = senha[i % senha_len]
+        senha_char = senha_str[i % senha_len]
         offset = ordem_cifra.index(char)  # Obtem o índice na ordem_cifra
         cifra_char = offset - int(senha_char)
         x = ordem_cifra[cifra_char % len(ordem_cifra)]
